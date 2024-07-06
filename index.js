6cec8d7885c2439f4e451e1cc2008fa9d22af6f0
Harsha Vardhan Raju Kondapalli <rajharsha666@gmail.com>
2024-07-07 00:43:42 +0530

    Added headers manually

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+  //allow access to current url. work for https as well
+  res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
+  res.removeHeader('x-powered-by');
+  //allow access to current method
+  res.setHeader('Access-Control-Allow-Methods',req.method);
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  next();
+})
+
 app.use(cors({
     origin: ["http://localhost:5173", 'https://emi-calculator-omega.vercel.app', 'https://emicalculatorjani.netlify.app', 'https://harshavkportfolio.netlify.app/'],
     methods: ['GET', 'POST'],
